6f14bf93cf0a1ea673c6461c3d109c23d60c4ec1
Christian Henkel <christian.henkel2@de.bosch.com>
2026-03-26 11:31:48 +0100

    detect skill with proper action call
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- tutorials/behavior_trees/bt_executor/include/pyrobosim_btcpp/nodes/detect_object_node.hpp
+++ tutorials/behavior_trees/bt_executor/include/pyrobosim_btcpp/nodes/detect_object_node.hpp
@@ -1,38 +1,45 @@
 #pragma once
 
 #include <behaviortree_ros2/bt_action_node.hpp>
-#include <pyrobosim_msgs/action/execute_task_action.hpp>
-#include "execute_task_node.hpp"
+#include <pyrobosim_msgs/action/detect_objects.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
 
 namespace BT
 {
 
-class DetectObject : public ExecuteTaskNode
+class DetectObject : public RosActionNode<pyrobosim_msgs::action::DetectObjects>
 {
 public:
-  DetectObject(const std::string& name, const NodeConfig& conf, const RosNodeParams& params)
-    : ExecuteTaskNode(name, conf, params)
+  DetectObject(
+    const std::string & name, const NodeConfig & conf,
+    const RosNodeParams & params)
+  : RosActionNode<pyrobosim_msgs::action::DetectObjects>(name, conf, params)
   {}
 
   // specify the ports offered by this node
   static BT::PortsList providedPorts()
   {
-    return ExecuteTaskNode::appendProvidedPorts({ BT::InputPort<std::string>("object_id") });
+    return ExecuteTaskNode::appendProvidedPorts({BT::OutputPort<std::string>(
+                 "object_id")});
   }
 
   // Implement the method that sends the goal
-  bool setGoal(TaskAction& action) override
+  bool setGoal(Goal & goal) override
   {
-    std::string object;
-    if(!getInput("object_id", object) || object.empty())
-    {
-      throw BT::RuntimeError("missing required input [object_id]");
-    }
-    // prepare the goal message
-    action.type = "detect";
-    action.object = object;
     return true;
   }
+
+  // Implement the method that processes the result
+  BT::NodeStatus onResultReceived(const WrappedResult & w_result) override
+  {
+    if (w_result.code == rclcpp_action::ResultCode::SUCCEEDED) {
+      setOutput("object_id", w_result.result->detected_objects[0].name);
+      return BT::NodeStatus::SUCCESS;
+    } else {
+      return BT::NodeStatus::FAILURE;
+    }
+  }
+
 };
 
 }  // namespace BT
