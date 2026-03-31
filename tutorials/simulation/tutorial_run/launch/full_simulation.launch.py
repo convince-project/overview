a89988d4c49891165062622164d3709683667b8b
Marco Lampacrescia <marco.lampacrescia@de.bosch.com>
2026-03-31 17:05:26 +0200

    Bugfix for detect_object_node
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- tutorials/simulation/tutorial_run/launch/full_simulation.launch.py
+++ tutorials/simulation/tutorial_run/launch/full_simulation.launch.py
@@ -1,6 +1,5 @@
-import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler, TimerAction
 from launch.event_handlers import OnProcessExit
 from launch.events import Shutdown
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
@@ -57,13 +56,22 @@ def generate_launch_description():
         name="btcpp_executor",
         output="screen",
         emulate_tty=True,
-        parameters=[{"tree": PathJoinSubstitution([LaunchConfiguration("policy_dir"), LaunchConfiguration("policy")])}],
+        parameters=[{"tree": PathJoinSubstitution(
+            [LaunchConfiguration("policy_dir"), LaunchConfiguration("policy")]
+        )}],
+        # prefix=['xterm -e gdb -ex=run --args'],
+        # prefix=['gdbserver localhost:3000']
     )
 
+    # Add a delay of 3 seconds, to make sure the launch test can catch the end state in time.
     shutdown_on_bt_exit = RegisterEventHandler(
         OnProcessExit(
             target_action=bt_executor,
-            on_exit=[EmitEvent(event=Shutdown(reason="btcpp_executor finished"))],
+            on_exit=[
+                TimerAction(
+                    period=3.0,
+                    actions=[EmitEvent(event=Shutdown(reason="btcpp_executor finished"))]
+                )],
         )
     )
 
