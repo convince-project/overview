7d61de92d143c5169244515214b3f2ad6a61d2a3
Marco Lampacrescia <65171491+MarcoLm993@users.noreply.github.com>
2026-04-15 14:18:30 +0200

    Small refinements to refine-plan (#46)
    Signed-off-by: Marco Lampacrescia <marco.lampacrescia@de.bosch.com>

--- tutorials/refine_plan_demo/bringup/start_sim.sh
+++ tutorials/refine_plan_demo/bringup/start_sim.sh
@@ -8,10 +8,12 @@ MONGO_CONNECTION_STRING=$2
 
 tmux -2 new-session -d -s $SESSION
 tmux rename-window -t $SESSION 'pyrobosim'
-tmux new-window -t $SESSION -n 'policy-executor'
+tmux split-window -t $SESSION -h
 
-tmux select-window -t $SESSION:pyrobosim
+tmux select-pane -t $SESSION:0.0
 tmux send-keys "ros2 run tutorial_sim run --ros-args -p detect_succ_prob:=1.0" C-m
 
-tmux select-window -t $SESSION:policy-executor
+tmux select-pane -t $SESSION:0.1
 tmux send-keys "ros2 launch refine_plan_demo policy_executor.launch.py db_collection:=demo-$POLICY_EXECUTOR_MODE mode:=$POLICY_EXECUTOR_MODE db_connection_string:=$MONGO_CONNECTION_STRING" C-m
+
+tmux attach-session -t $SESSION
